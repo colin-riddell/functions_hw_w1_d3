e5ece8d68a26c9b8163ac7a8285de181e984cf99
Colin J Riddell <colinjohnriddell@gmail.com>
2020-11-05 10:45:55 +0000

    Multiply working

--- src/python_functions_practice.py
+++ src/python_functions_practice.py
@@ -5,6 +5,10 @@ def return_10():
 def add(num1, num2):
     return num1 + num2
 
-
+# take num1 and num2, subtract and return result
 def subtract(num1, num2):
     return num1 - num2
+
+
+def multiply(num1, num2):
+    return num1 * num2
